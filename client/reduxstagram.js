439de180fe8b7762e3c1bda8bcad2f33d2fe2e0d
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-08-02 23:10:06 +0100

    add

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import css from './styles/style.styl';
+import css from "./styles/style.styl";
 
-import Main from './components/Main';
-import Single from './components/Single';
-import PhotoGrid from './components/PhotoGrid';
+import Main from "./components/Main";
+import Single from "./components/Single";
+import PhotoGrid from "./components/PhotoGrid";
 
-import {Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+
+import { Provider } from "react-redux";
+import store, { history } from "./store";
 
 const router = (
-    <Router history={browserHistory}>
-        <Route path='/' component={Main}>
-            <IndexRoute component={PhotoGrid}></IndexRoute>
-            <Route path='/view/:postId' component={Single}></Route>
-        </Route>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid} />
+        <Route path="/view/:postId" component={Single} />
+      </Route>
     </Router>
-)
+  </Provider>
+);
 
-render(router, document.getElementById('root'));
-
+render(router, document.getElementById("root"));
