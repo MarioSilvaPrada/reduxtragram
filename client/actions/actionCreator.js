493bafae2e62f1a03c0ce95ead9c7d7651d1ffbf
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-08-04 23:08:37 +0100

    finish

--- client/actions/actionCreator.js
+++ client/actions/actionCreator.js
@@ -1,24 +1,27 @@
-//increment
+// increment
 export function increment(index) {
   return {
-    type: "INCREMENT_LIKES",
+    type: 'INCREMENT_LIKES',
     index
-  };
+  }
 }
+
 // add comment
 export function addComment(postId, author, comment) {
   return {
-    type: "ADD_COMMENT",
+    type: 'ADD_COMMENT',
     postId,
     author,
     comment
-  };
+  }
 }
+
 // remove comment
-export function removeComment(postId, index) {
+
+export function removeComment(postId, i) {
   return {
-    type: "REMOVE_COMMENT",
-    index,
+    type: 'REMOVE_COMMENT',
+    i,
     postId
-  };
+  }
 }
